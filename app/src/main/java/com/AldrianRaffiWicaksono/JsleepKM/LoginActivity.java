f70ee146d360f8d6a3b2fe248c649b617796ab4a
Aldrian Raffi <aldrian.raffi@ui.ac.id>
2022-11-15 17:45:29 +0700

    First Android

---
+++ app/src/main/java/com/AldrianRaffiWicaksono/JsleepKM/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.AldrianRaffiWicaksono.JsleepKM;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView register = findViewById(R.id.register);
+        Button login = findViewById(R.id.loginButton);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent move = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(move);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent move = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(move);
+            }
+        });
+
+    }
+
+}
